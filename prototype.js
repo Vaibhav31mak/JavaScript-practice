ff256f41e5e654b7ed48f1304f4202ce1910df83
xyz <vaibhav.makwana@simformsolutions.com>
2026-02-20 13:23:42 +0530

    to check if the code is modified does the pull request cange?

---
+++ prototype.js
@@ -0,0 +1,35 @@
+const myDate = new Date();
+let object = myDate;
+
+do {
+  object = Object.getPrototypeOf(object);
+  console.log(object);
+} while (object);
+
+// Date.prototype
+// Object { }
+// null
+
+console.log(myDate.getTime()); // 819129600000
+
+myDate.getTime = function () {
+  console.log("something else!");
+};
+
+myDate.getTime(); // 'something else!'
+
+const personPrototype = {
+  greet() {
+    console.log(`hello, my name is ${this.name}!`);
+  },
+};
+
+function Person(name) {
+  this.name = name;
+}
+
+Object.assign(Person.prototype, personPrototype);
+// or
+// Person.prototype.greet = personPrototype.greet;
+const reuben = new Person("Reuben");
+reuben.greet(); // hello, my name is Reuben!
